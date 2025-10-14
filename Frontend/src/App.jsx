b287d479bf8e13939ff85169b5d912b1e11da8c2
BossEl-566 <datsomorelliot56@gmail.com>
2025-10-14 09:29:57 +0100

    feat: update React and add routing with Dashboard page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Signup from './pages/Signup';
 
 export default function App() {
   return (
-    <div className='text-4xl text-amber-700'>Linus Blog page</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/signup' element={<Signup/>} />
+      </Routes>
+      </BrowserRouter>
   )
 }

af97db8b7ba9a8983ed0beaf0ec2298d5e221ab5
BossEl-566 <datsomorelliot56@gmail.com>
2025-10-14 14:57:46 +0100

    feat: add dotenv and mongoose dependencies, configure MongoDB connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 
 
 const app = express();
+mongoose.connect(process.env.MONGODB).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+});
+
 
 app.listen(5000, () => {
   console.log('Server is running on http://localhost:5000');
